418a61726cf020cce63f15e1d218f6e021e383ec
muhammed swalih.t <salumuhammadswalih@mil.com>
2025-12-19 23:23:13 +0530

    updated base url for deployment

--- src/api.js
+++ src/api.js
@@ -1,35 +1,35 @@
-import axios from "axios"
+  import axios from "axios"
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 })
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token")
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  },
-)
+  api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token")
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    },
+  )
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      // Token expired or invalid
-      localStorage.removeItem("token")
-      window.location.href = "/login"
-    }
-    return Promise.reject(error)
-  },
-)
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        // Token expired or invalid
+        localStorage.removeItem("token")
+        window.location.href = "/login"
+      }
+      return Promise.reject(error)
+    },
+  )
 
-export default api
+  export default api
